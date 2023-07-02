25f68e77b2260108dc9e8491404210690801cad8
Quincy Kaka <quincykaka@192.168.1.126>
2023-07-02 19:17:42 +0100

    finalised concept version

--- shiny_concept.R
+++ shiny_concept.R
@@ -1,30 +1,24 @@
 # set up shiny
-install.packages("readxl")
 library(shiny)
 library(readxl)
-
-data <- read_xlsx("Daily Sales/test.xlsx")
+library(ggplot2)
+library(dplyr)
 
 # begin shiny
 library(shiny)
 
 ui <- fluidPage(
   # app title
-  titlePanel("Prrof of concept piece")
+  titlePanel("Proof of concept piece"),
   
-  # I want to split to a slider (L) and figure (R)
+  # I want to split to a dataset (L) and figure (R)
   sidebarLayout(
     
-    # a sidebar panel for input
+    # a sidebar panel for dataset
     sidebarPanel(
       # have a slider for top X brands
-      sliderInput(inputId = "topx",
-                  label = "Top X Brands with Net Sales",
-                  min = 1,
-                  max = 10,
-                  value = 3)
-      
-    )
+      tableOutput(outputId = "topx_table")
+    ),
     
     # main panel for output figure
     mainPanel(
@@ -32,11 +26,25 @@ ui <- fluidPage(
       plotOutput(outputId = "topx_plots")
     )
   )
-  
 )
 
 server <- function(input, output, session) {
   
+  # import the data
+  data <- read_xlsx("Daily Sales/test.xlsx")
+  # sort it by net sales and select key variables
+  data_o <- data %>% 
+    arrange(desc(`NET SALES`)) %>%
+    select(SKU, BRAND, `NET SALES`, MARGIN)
+  # filter top 3 rows = top 3 net sales
+  data_f <- data_o[2:4,]
+  
+  output$topx_table <- renderTable(data_f)
+  
+  output$topx_plots <- renderPlot({
+    ggplot(data = data_f, aes(x = BRAND, y = `NET SALES`, size = 2)) +
+      geom_point()
+  })
 }
 
 shinyApp(ui, server)
