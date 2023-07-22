cb445d6f851aee39c084719ba03e5a8d14600c68
Quincy Kaka <quincykaka@192.168.1.136>
2023-07-22 16:36:21 +0100

    arrange bug fixed using across() ggplot must be sorted at factor level

--- insight draft 1.R
+++ insight draft 1.R
@@ -4,6 +4,9 @@ library(readxl)
 library(ggplot2)
 library(dplyr)
 
+install.packages("forcats")
+library(forcats)
+
 # begin shiny
 library(shiny)
 
@@ -26,7 +29,7 @@ ui <- fluidPage(
         multiple = FALSE,
         width = '200px'),
         selectInput("metric", "METRIC:", choices = c(
-          "Net SALES" = "NET SALES",
+          "Net SALES" = "NET_SALES",
           "Margin" = "MARGIN"
         ), width = '200px'),
         textInput("top_x", "Top x:",
@@ -63,20 +66,28 @@ server <- function(input, output, session) {
   # filter top 5 rows/metric values
   
   output$table <- renderTable({
+    
+    in_met <- as.name(input$metric)
+    
     read_xlsx("Daily Sales/test.xlsx") %>%
       filter(TYPE == input$type) %>%
-      arrange(desc(input$metric)) %>%
       select(TITLE, BRAND, `NET SALES`, MARGIN) %>%
+      rename(NET_SALES = `NET SALES`) %>%
+      arrange(desc(across(input$metric))) %>%
       slice(1:input$top_x)})
   
   output$plot <- renderPlot({
+    
+    in_met <- as.name(input$metric)
+    
     ggplot(data = read_xlsx("Daily Sales/test.xlsx") %>%
              filter(TYPE == input$type) %>%
-             arrange(desc(input$metric)) %>%
              select(TITLE, BRAND, `NET SALES`, MARGIN) %>%
+             rename(NET_SALES = `NET SALES`) %>%
+             mutate(TITLE = fct_reorder(TITLE, desc(across(input$metric)))) %>%
              slice(1:input$top_x), 
-           aes(x = TITLE)) +
-      geom_col(aes( y = input$metric, color = BRAND, size = 2)) +
+           aes_string(x = "TITLE", y = input$metric, fill = "BRAND")) +
+      geom_col() +
       scale_size_continuous(guide = "none")
   })
   
