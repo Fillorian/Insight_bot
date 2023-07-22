b195c27a7d5f04b29e35593e566f2a8d82108873
Quincy Kaka <quincykaka@192.168.1.136>
2023-07-22 17:13:03 +0100

    plot bug near fixed, locate issue preventing plot loading.

--- insight draft 1.R
+++ insight draft 1.R
@@ -66,29 +66,27 @@ server <- function(input, output, session) {
   # filter top 5 rows/metric values
   
   output$table <- renderTable({
-    
-    in_met <- as.name(input$metric)
-    
     read_xlsx("Daily Sales/test.xlsx") %>%
       filter(TYPE == input$type) %>%
       select(TITLE, BRAND, `NET SALES`, MARGIN) %>%
       rename(NET_SALES = `NET SALES`) %>%
       arrange(desc(across(input$metric))) %>%
-      slice(1:input$top_x)})
+      slice(1:input$top_x)
+    })
   
   output$plot <- renderPlot({
-    
-    in_met <- as.name(input$metric)
-    
-    ggplot(data = read_xlsx("Daily Sales/test.xlsx") %>%
-             filter(TYPE == input$type) %>%
-             select(TITLE, BRAND, `NET SALES`, MARGIN) %>%
-             rename(NET_SALES = `NET SALES`) %>%
-             mutate(TITLE = fct_reorder(TITLE, desc(across(input$metric)))) %>%
-             slice(1:input$top_x), 
-           aes_string(x = "TITLE", y = input$metric, fill = "BRAND")) +
-      geom_col() +
-      scale_size_continuous(guide = "none")
+    ggplot(
+      data = read_xlsx("Daily Sales/test.xlsx") %>%
+        filter(TYPE == input$type) %>%
+        select(TITLE, BRAND, `NET SALES`, MARGIN) %>%
+        rename(NET_SALES = `NET SALES`) %>%
+        arrange(desc(across(input$metric))) %>%
+       # mutate(TITLE = factor(TITLE, levels = unique(TITLE))) %>%
+        slice(1:input$top_x),
+      aes_string(x = "TITLE", y = input$metric, fill = "BRAND") +
+        geom_col() +
+        scale_size_continuous(guide = "none") 
+    )
   })
   
   output$report <- renderText({
